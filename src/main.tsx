4ff903014162fa1a19d80e4bb070294b71d43864
charly <carlos.sanchez.carrasco2008@gmail.com>
2025-04-23 16:49:44 +0200

    updated2

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes";
 import "./styles/Home.css";
-
+import { Analytics } from "@vercel/analytics/react"
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
+        <Analytics />
         <AppRoutes />
     </React.StrictMode>
 );
